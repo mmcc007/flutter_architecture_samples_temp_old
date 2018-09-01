4c37e033730d8f67a154d357352153c108d8fad6
Maurice McCabe <mmcc007@gmail.com>
2018-09-01 14:50:55 -0700

    Added driver test for ios and android with fast finish for unit tests

--- script/runDriver.sh
+++ script/runDriver.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-# runs build for a package that has a lib/main.dart
-runBuild () {
+# runs driver for a package that has a lib/main.dart
+runDriver () {
   cd $1;
-  #if grep flutter pubspec.yaml > /dev/null 2>&1; then
   if [ -f "lib/main.dart" ]; then
-    echo "running build in $1"
+    echo "running driver in $1"
     # check if build_runner needs to be run
-    # todo: fix build in ./example/built_redux
+    # todo: fix build_runner in ./example/built_redux
     if grep build_runner pubspec.yaml > /dev/null  && [ "$1" != "./example/built_redux" ]; then
       flutter packages get
       flutter packages pub run build_runner build --delete-conflicting-outputs
     fi
-    flutter build apk
+    flutter driver test_driver/todo_app.dart
   fi
 }
 
-export -f runBuild
+export -f runDriver
 
 # expects to find most packages at second directory level
-find . -maxdepth 2 -type d -exec bash -c 'runBuild "$0"' {} \;
+find . -maxdepth 2 -type d -exec bash -c 'runDriver "$0"' {} \;
 # find exits with 0
